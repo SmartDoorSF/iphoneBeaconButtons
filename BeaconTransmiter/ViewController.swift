076152bec65352496a90188b0e5d741d39c42aab
Ryan Jones <ryan.j.jones10@gmail.com>
2016-04-18 20:45:18 -0700

    buttons for unlock and lock working

--- BeaconTransmiter/ViewController.swift
+++ BeaconTransmiter/ViewController.swift
@@ -7,19 +7,101 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBPeripheralManagerDelegate {
+    
+    @IBOutlet var uuid: UILabel!
+    @IBOutlet var major: UILabel!
+    @IBOutlet var minor: UILabel!
+    @IBOutlet var identity: UILabel!
+    @IBOutlet var beaconStatus: UILabel!
+    
+    var localBeacon: CLBeaconRegion!
+    var beaconPeripheralData: NSDictionary!
+    var peripheralManager: CBPeripheralManager!
+    var isBroadcasting = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func initLocalBeacon(minorNum : Int) {
+        if localBeacon != nil {
+            stopLocalBeacon()
+        }
+        
+        let localBeaconUUID = "66dae67d-22e2-466b-b7d6-7093d52ceeb7"
+        let localBeaconMajor: CLBeaconMajorValue = 8127
+        let localBeaconMinor: CLBeaconMinorValue = UInt16(minorNum)
+        
+        let uuid = NSUUID(UUIDString: localBeaconUUID)!
+        localBeacon = CLBeaconRegion(proximityUUID: uuid, major: localBeaconMajor, minor: localBeaconMinor, identifier: "MySmartDoor")
+        
+        beaconPeripheralData = localBeacon.peripheralDataWithMeasuredPower(nil)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
+    }
+    
+    func stopLocalBeacon() {
+        peripheralManager.stopAdvertising()
+        peripheralManager = nil
+        beaconPeripheralData = nil
+        localBeacon = nil
+    }
+    
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager) {
+        if peripheral.state == .PoweredOn {
+            peripheralManager.startAdvertising(beaconPeripheralData as! [String: AnyObject]!)
+            beaconStatus.text = "Transmitting!"
+        } else if peripheral.state == .PoweredOff {
+            peripheralManager.stopAdvertising()
+            self.beaconStatus.text = "Power Off"
+        }
+    }
+    
+    func updateInterface(minorNum: Int){
+        
+        let localBeaconUUID = "66dae67d-22e2-466b-b7d6-7093d52ceeb7"
+        let localBeaconMajor: CLBeaconMajorValue = 8127
+        let localBeaconMinor: CLBeaconMinorValue = UInt16(minorNum)
+        
+        self.uuid.text = "\(localBeaconUUID)"
+        self.major.text = "\(localBeaconMajor)"
+        self.minor.text = "\(localBeaconMinor)"
+        self.identity.text = "MySmartDoor"
+    }
+    
+    @IBAction func transmitBeacon(sender: AnyObject) {
+        if !isBroadcasting {
+            initLocalBeacon(1)
+            isBroadcasting = true
+            updateInterface(1)
+        }
+        else {
+            stopLocalBeacon()
+            isBroadcasting = false
+        }
+    }
+    
+    @IBAction func StopTransmit(sender: AnyObject) {
+        stopLocalBeacon()
+        isBroadcasting = false
+        updateInterface(5)
+    }
+    
+    @IBAction func unlockDoor(sender: AnyObject) {
+        stopLocalBeacon()
+        initLocalBeacon(2)
+        isBroadcasting = true
+        updateInterface(2)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func lockDoor(sender: AnyObject) {
+        stopLocalBeacon()
+        initLocalBeacon(3)
+        isBroadcasting = true
+        updateInterface(3)
     }
-
-
 }
 
